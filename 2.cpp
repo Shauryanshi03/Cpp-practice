467be99a468ab46c640652860ed21e858f488d95
Shauryanshi03 <shauryanshitripathi2005@gmail.com>
2026-04-21 21:18:30 +0530

    Add files via upload
    A simple C++ program showcasing the implementation of different operators.

---
+++ 2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a=5;
+    char ch='a';
+    
+    cout<<a<<endl;
+    cout<<ch<<endl;
+
+    int b=3;
+
+    int add=a+b;
+    cout<<add<<endl;
+
+    int sub=a-b;
+    cout<<sub<<endl;
+
+    int mul=a*b;
+    cout<<mul<<endl;
+
+    int div=a/b;
+    cout<<div<<endl;
+
+    int rem=a%b;
+    cout<<rem<<endl;
+
+    int c=4;
+    int d=7;
+
+    bool rel=c<=d;
+    cout<<rel<<endl;
+    bool re2=c>d;
+    cout<<re2<<endl;
+
+    return 0;
+}
