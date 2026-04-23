f268333184837e87b0519afaebf17c99ce92e179
Shauryanshi <shauryanshitripathi2005@gmail.com>
2026-04-23 10:46:24 +0530

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    char ch;
+    cout<<"enter any character:";
+    cin>>ch;
+
+    if(ch>=97 && ch<=122){
+        cout<<"this is lowercase";
+    }
+    else if(ch>=65 && ch<=90){
+        cout<<"this is uppercase";
+    }
+    else if(ch>=48 && ch<=57){
+        cout<<"this is numeric";
+    }
+}
