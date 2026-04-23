7795991d16aa612e0eeffaed4504c2bd5ab83cdc
Shauryanshi <shauryanshitripathi2005@gmail.com>
2026-04-23 10:42:39 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter a number: ";
+    cin>>a;
+
+    if(a>0){
+        cout<<"number is positive"<<endl;
+    }
+
+    else if(a<0){
+        cout<<"number is negative"<<endl;
+    }
+
+    else{
+        cout<<"number is 0"<<endl;
+    }
+
+}
